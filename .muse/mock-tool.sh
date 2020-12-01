370d8ef92acf9e0a1a75ff27b7a1e8f83313dba7
Vincent Khougaz <vincent@muse.dev>
2020-12-01 12:51:47 -0800

    Creating a mock tool that can emit custom types/counts based on file contents

---
+++ .muse/mock-tool.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+dir=$1
+commit=$2
+cmd=$3
+
+function version() {
+	echo 1
+}
+
+function applicable() {
+	echo "true"
+}
+
+function run() {
+	grep -rnP "^$BUG_PATTERN$" $dir \
+	  | sed -E "s/$FILE_PATTERN/$SUB_EXPRESSION/g" \
+		| sed '1s/^/[\n/; $!s/$/,/; $s/$/\n]/'
+}
+
+BUG_PATTERN="#bug:(.*?) - (.*)"
+FILE_PATTERN="(.*?):([0-9]*):$BUG_PATTERN"
+SUB_EXPRESSION='{"file": "\1","line": "\2","type": "\3","message": "\4"}'
+
+if [[ "$cmd" = "run" ]] ; then
+	run
+fi
+if [[ "$cmd" = "applicable" ]] ; then
+	applicable
+fi
+if [[ "$cmd" = "version" ]] ; then
+	version
+fi
